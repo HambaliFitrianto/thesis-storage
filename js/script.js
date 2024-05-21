47939c0438c365410c34fcf3eade5ab3d29f8c78
HambaliFitrianto <balcoy823@gmail.com>
2024-05-22 04:53:22 +0700

    update terbaru

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     const fileList = document.getElementById('fileList');
+    const photoGallery = document.getElementById('photoGallery');
+
     const files = [
-        { name: 'Word Skripsi.docx', path: 'files/word-skripsi.docx' },
-        { name: 'Pdf Skripsi.pdf', path: 'files/pdf-skripsi.pdf' },
-        { name: 'PPT Proposal Skripsi.pptx', path: 'files/ppt-proposal-skripsi.pptx' },
-        { name: 'PPT Skripsi.pptx', path: 'files/ppt-skripsi.pptx' },
-        { name: 'Perhitungan Manual.xlsx', path: 'files/perhitungan-manual.xlsx' }
+        { name: 'Word, Thesis.docx', path: 'files/word-skripsi.docx' },
+        { name: 'Pdf, Thesis.pdf', path: 'files/pdf-skripsi.pdf' },
+        { name: 'PPT, Thesis Proposal.pptx', path: 'files/ppt-proposal-skripsi.pptx' },
+        { name: 'PPT, Thesis.pptx', path: 'files/ppt-skripsi.pptx' },
+        { name: 'Manual Calculation.xlsx', path: 'files/perhitungan-manual.xlsx' }
+    ];
+
+    const photos = [
+        { name: 'Proposal Seminar', path: 'img/seminar-proposal.jpg' },
+        { name: 'Thesis Guidance and Mbkm Research in the Lecturer Room', path: 'img/bimbingan-skripsi-dan-mbkm-riset-di-ruang-dosen.jpg' },
+        { name: 'Thesis Guidance & Mbkm Research in the Classroom', path: 'img/bimbingan-skripsi-dan-mbkm-riset-di-ruang-kelas.jpg' },
+        { name: 'Two Researcher Friends Mbkm', path: 'img/dua-teman-mbkm-riset.jpg' },
+        { name: 'Completed The Thesis Trial', path: 'img/selesai-sidang-taman-kampus.jpg' }
     ];
 
     const icons = {
@@ -38,5 +48,22 @@ document.addEventListener('DOMContentLoaded', function() {
         listItem.appendChild(link);
 
         fileList.appendChild(listItem);
+    });
+
+    photos.forEach(photo => {
+        const imgContainer = document.createElement('div');
+        imgContainer.className = 'photo-item';
+
+        const img = document.createElement('img');
+        img.src = photo.path;
+        img.alt = photo.name;
+        imgContainer.appendChild(img);
+
+        const name = document.createElement('p');
+        name.textContent = photo.name;
+        name.style.fontSize = '14px'; // Atur ukuran teks di sini
+        imgContainer.appendChild(name);
+
+        photoGallery.appendChild(imgContainer);
     });
 });
